378cf2ca2a6bdd2b92db452cb23c1ddf7ed21ad0
Mariia <mariia.bieskova@gmail.com>
2024-05-03 13:51:06 +0300

    complete js exercises

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(farenheit) {
+  resultCelsius = (farenheit - 32) / 1.8;
+
+  return Math.round(resultCelsius * 10)/10;
+
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  resultFarenheit = celsius * 1.8 + 32;
+  return Math.round(resultFarenheit * 10)/10;
 };
 
 // Do not edit below this line
