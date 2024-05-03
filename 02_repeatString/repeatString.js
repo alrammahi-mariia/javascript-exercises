4e9f956ea625339751315d1271e8f3888c7c74dd
Mariia <mariia.bieskova@gmail.com>
2024-05-03 12:59:30 +0300

    complete js exercises

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,12 @@
-const repeatString = function() {
 
-};
+const repeatString = function(string, times) {
+    if (times < 0) return "ERROR";
+    let newStr = "";
+        for (num = 0; num < times; num++){
+            newStr += string 
+    };
 
+    return newStr;
+}
 // Do not edit below this line
 module.exports = repeatString;
